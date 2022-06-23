e4a08374fda166b455e7d0dccad013c10e75bda7
Nikhil921 <78449908+Nikhil921@users.noreply.github.com>
2022-06-23 01:42:06 -0400

    final commit

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,16 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+//direct to the projects page
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+//to go the about page 
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'about' });
+});
+//direct to the contact page
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'contact' });
+});
 module.exports = router;
